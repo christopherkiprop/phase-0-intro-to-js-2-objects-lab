0c32b10631d077671919f8db90efcf6e66fac5cd
christopherkiprop <choppachepa@gmail.com>
2024-07-12 23:51:40 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+    streetAddress: '123 Main St'
+  };
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+ 
